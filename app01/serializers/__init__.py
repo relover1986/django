a5ca0b76fff4004c3d0b0926e213b99662f6cfab
孙宏晨 <shc1986@me.com>
2026-06-02 00:52:00 +0800

    fix: serializers/__init__.py 相对导入改为 app01.models

--- app01/serializers/__init__.py
+++ app01/serializers/__init__.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import (
+from app01.models import (
     Admin, QuestionType, JskjgQuestion, WxpzxQuestion,
     UserAnswer, Tihao, LoginRecords, ExplosiveInventoryItem,
     CategoryContent, UploadedPDF, UploadedTu, UploadedZhaopian,
