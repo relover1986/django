12a2fa6f8a53326670cba66f08e617f289a4b673
孙宏晨 <shc1986@me.com>
2026-06-14 01:00:01 +0800

    [auto] 服务器自动备份 2026-06-14 01:00 (2 文件变更)

--- app01/models/quiz.py
+++ app01/models/quiz.py
@@ -14,6 +14,7 @@ class Question(models.Model):
     question = models.TextField(verbose_name="题目")
     options = models.CharField(max_length=200, verbose_name="选项")
     correct_answer = models.CharField(max_length=20, verbose_name="正确答案")
+    analysis = models.TextField(verbose_name="解析", blank=True, default="")
 
     class Meta:
         db_table = "app01_question"
@@ -25,6 +26,3 @@ class UserAnswer(models.Model):
     tihao = models.CharField(max_length=20)
     date = models.DateField()
     ident = models.CharField(max_length=20)
-
-    # def __str__(self):
-    #     return self.UserAnswer
